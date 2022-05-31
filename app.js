c8ef981e0ed91f32ba2c5b25ff6e865b3f7c06ba
darpan9460@gmail.com <darpan9460@gmail.com>
2022-06-01 03:13:22 +0530

    Added Comments to improve readablity and understanding the use of code

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
+                                                // Mongoose Cheatsheet
+
+
+// Importing & Connecting Mongoose with mongodb database
 const mongoose =require("mongoose")
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
+
+// Creating Schema 
 const fruitSchema= new mongoose.Schema({
     name: String,
     rating: Number,
     review: String
 });
+const personSchema= new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+
+// Creating Model 
 const Fruit = mongoose.model("Fruits",fruitSchema);
+const Person = mongoose.model("Person",personSchema);
+
+
+// Creating Documents with the help of Model 
 const fruit = new Fruit({
     name:"Apple",
     rating:7,
     review:"An a apple a day keeps a doctor away"
 }
 );
+const person = new Person({
+    name:"Darpan",
+age:19
+})
+
+
+
+// Saving the document in Database
+fruit.save();
+// person.save()
+
+
+// Inserting many Documents at Same Time in a database
 const Mango = new Fruit({
     name:"Mango",
     rating:8,
@@ -24,24 +54,15 @@ const Orange = new Fruit({
     review:"Sour but healthy"
 }
 );
-// fruit.save();
-// Fruit.insertMany([Mango,Orange],function(err){
-//     if(err)
-//     console.log(err);
-//     else
-//     console.log("Successfully inserted fruits");
-// })
-
-const personSchema= new mongoose.Schema({
-    name: String,
-    age: Number
-});
-const Person = mongoose.model("Person",personSchema);
-const person = new Person({
-    name:"Darpan",
-age:19
+Fruit.insertMany([Mango,Orange],function(err){
+    if(err)
+    console.log(err);
+    else
+    console.log("Successfully inserted fruits");
 })
-// person.save()
+
+
+// Deleting Many documents
 Person.deleteMany({name:"Darpan"},function(err){
     if(err)
     console.log(err);
@@ -50,11 +71,14 @@ Person.deleteMany({name:"Darpan"},function(err){
 });
 person.save()
 
+
+// Displaying Data 
 Fruit.find(function(err,fruits){
     if(err)
     console.log(err);
     else{
-        
+
+// Closing Connection 
 mongoose.connection.close(function(){
     console.log('Mongoose default connection disconnected');
     });
